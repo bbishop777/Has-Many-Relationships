8513ec44414ba3650f35646f9875ff2c38249565
BBishop <brad.bishop68@gmail.com>
2016-01-22 19:05:02 -1000

    Completes query exercise #13 from readme

--- joins.sql
+++ joins.sql
@@ -78,3 +78,18 @@ FROM users
 INNER JOIN comments ON comments.user_id= users.id
 INNER JOIN posts ON comments.post_id= posts.id
 WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
+
+SELECT users.first_name AS "post_author_first_name", users.last_name AS "post_author_last_name", posts.title AS "post_title",
+comment_author.username AS "comment_author_username", comments.body
+FROM users
+INNER JOIN posts ON posts.user_id= users.id
+INNER JOIN comments ON comments.user_id= posts.user_id
+INNER JOIN users AS comment_author ON users.id = comments.user_id
+WHERE (comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%') AND posts.content LIKE '%nemo%';
+
+SELECT count(comments.body, posts.content)
+FROM users
+INNER JOIN posts ON posts.user_id= users.id
+INNER JOIN comments ON comments.user_id= posts.user_id
+INNER JOIN users AS comment_author ON users.id = comments.user_id
+WHERE (comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%') AND posts.content LIKE '%nemo%';
