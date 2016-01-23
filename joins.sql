fecb0813d8c2a369e9b053d4cfb84902991ec0d2
BBishop <brad.bishop68@gmail.com>
2016-01-22 14:36:22 -1000

    Finishes up to number 5 creating queries on the database

--- joins.sql
+++ joins.sql
@@ -12,4 +12,21 @@ ALTER DATABASE has_many_blogs OWNER TO has_many_user;
 
 \i has_many_blogs.sql;
 
-SELECT * FROM users;
+-- SELECT * FROM users;
+
+-- SELECT * FROM posts WHERE user_id= 100;
+
+-- SELECT posts.*, users.first_name, users.last_name
+-- FROM posts
+-- INNER JOIN users ON posts.user_id= users.id
+-- WHERE users.id= 200;
+
+-- SELECT posts.*, users.username
+-- FROM posts
+-- INNER JOIN users on posts.user_id = users.id
+-- WHERE users.first_name= 'Norene' AND users.last_name= 'Schmitt';
+
+SELECT users.username, posts.created_at
+FROM users
+INNER JOIN posts on posts.user_id= users.id
+WHERE posts.created_at> '2015_01-01';
