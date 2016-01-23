007eb12225f6d2046476babc2ccbf49d0e3be6da
BBishop <brad.bishop68@gmail.com>
2016-01-22 15:27:01 -1000

    Completes exercise #7 on queries tasks from readme

--- joins.sql
+++ joins.sql
@@ -12,19 +12,19 @@ ALTER DATABASE has_many_blogs OWNER TO has_many_user;
 
 \i has_many_blogs.sql;
 
--- SELECT * FROM users;
+SELECT * FROM users;
 
--- SELECT * FROM posts WHERE user_id= 100;
+SELECT * FROM posts WHERE user_id= 100;
 
--- SELECT posts.*, users.first_name, users.last_name
--- FROM posts
--- INNER JOIN users ON posts.user_id= users.id
--- WHERE users.id= 200;
+SELECT posts.*, users.first_name, users.last_name
+FROM posts
+INNER JOIN users ON posts.user_id= users.id
+WHERE users.id= 200;
 
--- SELECT posts.*, users.username
--- FROM posts
--- INNER JOIN users on posts.user_id = users.id
--- WHERE users.first_name= 'Norene' AND users.last_name= 'Schmitt';
+SELECT posts.*, users.username
+FROM posts
+INNER JOIN users on posts.user_id = users.id
+WHERE users.first_name= 'Norene' AND users.last_name= 'Schmitt';
 
 SELECT users.username, posts.created_at
 FROM users
@@ -35,3 +35,7 @@ SELECT posts.title, posts.content, users.username, users.created_at
 FROM posts
 INNER JOIN users on posts.user_id= users.id
 WHERE users.created_at< '2015_01_01';
+
+SELECT comments.*, posts.title AS "Post Title"
+FROM comments
+INNER JOIN posts on comments.post_id= posts.id;
